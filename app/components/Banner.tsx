0f0a35036486a90f465615d374edfc07086380ba
Anastasia <anastasia.hrynkevich@outlook.com>
2025-05-11 21:12:22 +0100

    removed unneeded styles

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -16,7 +16,7 @@ export default function Banner() {
     return (
         <>
             {isHomePage && (
-                <div className={`w-full ${closeBanner ? 'flex' : 'hidden animate-fade-down animate-once animate-duration-[5000ms] animate-reverse'} justify-center items-center bg-[#F77429] relative py-3`}>
+                <div className={`w-full ${closeBanner ? 'flex' : 'hidden'} justify-center items-center bg-[#F77429] relative py-3`}>
                     <p className="text-[14px] text-[#1E1E1E] text-center px-[25px] mr-[25px]">The Printforge project was built using React, Next.js, TypeScript and Tailwind to create a dynamic and user-friendly experience.
                         You can view the source code on <Link className="underline decoration-1 font-semibold" href="https://github.com/nastiko/next-printforge">GitHub</Link> for a deeper look.</p>
                     <div onClick={handleCloseBanner} className="flex items-center absolute inset-y-0 right-0 py-3 pr-[25px] cursor-pointer">
